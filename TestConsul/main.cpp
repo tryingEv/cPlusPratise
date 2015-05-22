8397e94a0dc7af419727183f64e9ded55d046f23
吴勇均 <wuyongjun@wuyongjundeMacBook-Pro.local>
2015-05-23 01:00:05 +0800

    Initial Commit

---
+++ TestConsul/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestConsul
+//
+//  Created by 吴勇均 on 15/5/23.
+//  Copyright (c) 2015年 吴勇均. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
